2115f3aa789137e548245cb7c85132f2958cb805
Jean Georges Perrin <jgp@jgp.net>
2017-06-25 08:04:25 -0400

    After init code gen

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# net.jgp.labs.informix2spark
+# net.jgp.labs.informix2spark.l010.informix2spark
 Illustrates how to transfer IBM Informix data to Apache Spark using JDBC.
---
+++ src/main/java/net/jgp/labs/informix2spark/l010/App.java
@@ -0,0 +1,13 @@
+package net.jgp.labs.informix2spark.l010;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
